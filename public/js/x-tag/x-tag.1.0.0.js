8855528fa636601b9b110b6abd048d17094da2ea
Arron Schaar <aschaar@gmail.com>
2013-02-27 14:27:29 -0800

    Updated x-tag core lib

--- public/js/x-tag/x-tag.1.0.0.js
+++ public/js/x-tag/x-tag.1.0.0.js
@@ -303,6 +303,7 @@ if (!(document.register || {}).__polyfill__){
       return {
         base: 'OverflowEvent' in win ? 'overflowchanged' : type + 'flow',
         condition: function (custom, event) {
+          event.flow = type;
           return event.type == (type + 'flow') ||
           ((event.orient === 0 && event.horizontalOverflow == flow) ||
           (event.orient == 1 && event.verticalOverflow == flow) ||
@@ -587,7 +588,7 @@ if (!(document.register || {}).__polyfill__){
         attr = '#' + guid + ' > ';
       selector = attr + (selector + '').replace(',', ',' + attr, 'g');
       var result = element.parentNode.querySelectorAll(selector);
-      if (!id) element.id = null;
+      if (!id) element.removeAttribute('id');
       return xtag.toArray(result);
     },
 
@@ -606,34 +607,36 @@ if (!(document.register || {}).__polyfill__){
   /*** Pseudos ***/
 
     applyPseudos: function(key, fn, element) {
-      var last = { action: fn };
+      var listener = fn;
       if (key.match(':')) {
         var split = key.match(/(\w+(?:\([^\)]+\))?)/g),
             i = split.length;
         while (--i) {
           split[i].replace(/(\w*)(?:\(([^\)]*)\))?/, function (match, name, value) {
             var pseudo = xtag.pseudos[name];
+                pseudo.key = key;
+                pseudo.name = name;
+                pseudo.value = value;
             if (!pseudo) throw "pseudo not found: " + name;
-            var obj = {
-                  key: key,
-                  name: name,
-                  value: value,
-                  onAdd: pseudo.onAdd,
-                  listener: last.action,
-                  action: function(){
-                    var args = xtag.toArray(arguments);
-                    var returned = pseudo.action.apply(this, [obj].concat(args));
-                    return (returned === false) ? false : obj.listener.apply(this, args);
-                  }
-                };
-            last.action = obj.action;
+            var last = listener;
+            listener = function(){
+              var args = xtag.toArray(arguments),
+                  obj = {
+                    key: key,
+                    name: name,
+                    value: value,
+                    listener: last
+                  };
+              if (pseudo.action.apply(this, [obj].concat(args)) === false) return false;
+              return obj.listener.apply(this, args);
+            };
             if (element && pseudo.onAdd) {
-              element.getAttribute ? pseudo.onAdd.call(element, obj) : element.push(obj);
+              element.getAttribute ? pseudo.onAdd.call(element, pseudo) : element.push(pseudo);
             }
           });
         }
       }
-      return last.action;
+      return listener;
     },
 
   /*** Events ***/
