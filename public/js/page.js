0e8567075d845be5920f304c95e216b7dce00e7b
Arron Schaar <aschaar@gmail.com>
2013-03-04 15:41:55 -0800

    improved header to be closer to x-tags.org

--- public/js/page.js
+++ public/js/page.js
@@ -1,13 +1,5 @@
 (function(){
 	
-	var instructions = new ContentBox($('instructions').getFirst(), { wrap: $('instructions') });
-	window.addEvents({
-		'click:relay(#toggleInstructions)': function(){
-			var shown = $('instructions').getStyle('height') == 'auto';			
-			instructions[shown ? 'collapse' : 'show']();
-		}
-	});
-
 	hljs.tabReplace = '    ';
 	hljs.initHighlightingOnLoad();
 	
